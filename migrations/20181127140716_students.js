6292144e16d1e3da7bc46ae343fdd860c700e994
unknown <w.andrew.pedersen@gmail.com>
2018-11-27 14:23:20 -0700

    Knex migrate and rollback funcs added

--- migrations/20181127140716_students.js
+++ migrations/20181127140716_students.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("Students", student => {
+    student.increments("id"),
+      student.string("name"),
+      student.string("fav_animal"),
+      student.string("previous_occupation"),
+      student.float("hometown_lat", 8, 8),
+      student.float("hometown_long", 8, 8),
+      student.string("useless_superpower");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists("table_name");
 };
