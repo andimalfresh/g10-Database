408109b93f8b4dfe1b30ec46901390b472b07239
unknown <w.andrew.pedersen@gmail.com>
2018-11-27 09:48:50 -0700

    route worked

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 const port = 6661;
 
 app.listen(port);
-// () => {
-//     console.log(`listening on ${port}`)
-// }
+() => {
+  console.log(`listening on ${port}`);
+};
+
+app.get("/", (req, res) => {
+  res.send("THE ROUTE WORKED!");
+});
